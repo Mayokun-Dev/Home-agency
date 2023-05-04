4f8a678732fd1833b19c536cc87c6c4f7f180758
SukkyJoe <95610078+SukkyJoe@users.noreply.github.com>
2023-05-04 15:14:50 +0100

    Add files via upload

---
+++ Home rental agency/script.js
@@ -0,0 +1,42 @@
+// var hamburgerMenu = document.getElementsByClassName("hamburger-menu");
+// var menuToggle = document.querySelector(".menu-toggle");
+// var menu = document.getElementById("menu-right");
+
+
+
+// var toggle= window.getComputedStyle(menu).display
+// menuToggle.addEventListener("click", function(){
+//     // alert("YES")
+//     menu.classList.remove("menu-right")
+// })
+
+// var showMore = () => {menu.style.display = "initial"};
+// var hideMore = () => {menu[i].style.display = "none"};
+
+// if (showMore = false ){
+//     for(i = 0; i < menu.length; i++){
+//         hamburgerMenu[i].addEventListener("click", function(){
+//             for(i = 0; i < menu.length; i++)showMore()
+//         })
+//     };
+// }
+// else{
+//     for(i = 0; i < menu.length; i++){
+//         hamburgerMenu[i].addEventListener("click", function(){
+//             for(i = 0; i < menu.length; i++)hideMore()
+//         })
+//     };
+// }
+
+
+
+
+
+// function myFunction() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "flex-container") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "topnav";
+//     }
+//   }
